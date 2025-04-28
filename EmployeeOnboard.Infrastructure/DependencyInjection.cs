6508d4833045d48b89d4104ba2354c1fab949d6b
MacThuo <mercythuo17@gmail.com>
2025-04-28 11:38:24 +0300

    Added a background service to retry failed emails

--- EmployeeOnboard.Infrastructure/DependencyInjection.cs
+++ EmployeeOnboard.Infrastructure/DependencyInjection.cs
@@ -2,8 +2,10 @@
 using EmployeeOnboard.Application.Interfaces.RepositoryInterfaces;
 using EmployeeOnboard.Application.Interfaces.ServiceInterfaces;
 using EmployeeOnboard.Application.Interfaces.UOW;
+using EmployeeOnboard.Application.Shared;
 using EmployeeOnboard.Infrastructure.Repositories;
 using EmployeeOnboard.Infrastructure.Services;
+using EmployeeOnboard.Infrastructure.Services.BackgroundJobs;
 using EmployeeOnboard.Infrastructure.Services.Initilization;
 using EmployeeOnboard.Infrastructure.Services.Notification;
 using EmployeeOnboard.Infrastructure.Services.PasswordManagementService;
@@ -28,9 +30,8 @@ namespace EmployeeOnboard.Infrastructure
             services.AddScoped<IChangePassword, ChangePasswordService>();
             services.AddScoped<IForgotPasswordService, ForgotPasswordService>();
             services.AddScoped<IForgotPasswordTokenRepository, ForgotPasswordTokenRepository>();
-
-
-            // Register EmailTemplateService
+            services.AddScoped<IEmailRetryProcessor, EmailRetryProcessor>();
+            services.AddScoped<IEmailLogQueryService, EmailLogQueryService>();
             services.AddScoped<EmailTemplateService>();
             services.AddScoped<IEmailRetryService, EmailRetryService>();
             services.AddScoped<IEmailLogQueryService, EmailLogQueryService>();
@@ -42,6 +43,8 @@ namespace EmployeeOnboard.Infrastructure
             // register the initializer class
             services.AddScoped<DbInitializer>();
 
+            //Hosted Services
+            services.AddHostedService<EmailRetryBackgroundService>();
 
             // Register SmtpClientWrapper for dependency injection
             services.AddTransient<ISmtpClientWrapper>(provider =>
